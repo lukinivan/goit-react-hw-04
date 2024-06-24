495446a3e64063ec1eb6e9b37bf06ec471569866
lukinivan <Lukinivan09@gmail.com>
2024-06-24 13:31:00 +0100

    Home work done

---
+++ src/components/ImageModal/ImageModal.jsx
@@ -0,0 +1,33 @@
+import Modal from "react-modal";
+Modal.setAppElement("#root");
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    width: "80%",
+    padding: "5px",
+  },
+  overlay: {
+    backgroundColor: "rgba(40, 40, 40, 0.75)",
+  },
+};
+
+export const ImageModal = ({ isModalOpen, closeModal, selectedImage }) => {
+  return (
+    <div>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <img src={selectedImage?.regular} alt={selectedImage?.description} />
+      </Modal>
+    </div>
+  );
+};
